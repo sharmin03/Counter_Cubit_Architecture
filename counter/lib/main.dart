4e49b7948abd9328be0762a8792de7100613f5bd
Sharmin Khan <sharminaftab03@gmail.com>
2021-08-06 16:36:50 +0200

    Implementing Bloc consumer to show snackbar on each button click

--- counter/lib/main.dart
+++ counter/lib/main.dart
@@ -39,48 +39,68 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'This is the counter value:',
-            ),
-            BlocBuilder<CounterCubit, CounterState>(
-              builder: (context, state) {
-                return Text(
-                  state.counterValue.toString(),
-                  style: Theme.of(context).textTheme.headline4,
-                );
-              },
-            ),
-            SizedBox(
-              height: 24,
-            ),
-            Row(
+      body: BlocConsumer<CounterCubit, CounterState>(
+        listener: (context, state) {
+          final wasIncremented = state.wasIncremented;
+          if (wasIncremented != null) {
+            if (wasIncremented) {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text('Incremented'),
+                duration: Duration(milliseconds: 300),
+              ));
+            } else {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text('Decremented'),
+                duration: Duration(milliseconds: 300),
+              ));
+            }
+          }
+        },
+        builder: (context, state) {
+          return Center(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).decrement();
+              children: <Widget>[
+                Text(
+                  'This is the counter value:',
+                ),
+                BlocBuilder<CounterCubit, CounterState>(
+                  builder: (context, state) {
+                    return Text(
+                      state.counterValue.toString(),
+                      style: Theme.of(context).textTheme.headline4,
+                    );
                   },
-                  tooltip: 'Increment',
-                  child: Icon(Icons.remove),
                 ),
                 SizedBox(
-                  width: 24,
+                  height: 24,
                 ),
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).increment();
-                  },
-                  tooltip: 'Increment',
-                  child: Icon(Icons.add),
-                )
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FloatingActionButton(
+                      onPressed: () {
+                        BlocProvider.of<CounterCubit>(context).decrement();
+                      },
+                      tooltip: 'Increment',
+                      child: Icon(Icons.remove),
+                    ),
+                    SizedBox(
+                      width: 24,
+                    ),
+                    FloatingActionButton(
+                      onPressed: () {
+                        BlocProvider.of<CounterCubit>(context).increment();
+                      },
+                      tooltip: 'Increment',
+                      child: Icon(Icons.add),
+                    )
+                  ],
+                ),
               ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
